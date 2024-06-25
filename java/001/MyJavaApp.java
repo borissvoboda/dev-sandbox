3fe183156807f4a896d8d0afad8446baad5c11c7
borissvoboda <svoboda.boris@gmail.com>
2024-06-25 22:42:16 +0200

    java001

---
+++ java/001/MyJavaApp.java
@@ -0,0 +1,9 @@
+class MyJavaApp {
+    public static void main(String[] args) {
+        String hello = "Hello World!";
+    }
+
+    public static String sayHello() {
+        return "Hello World!";
+    }
+}
