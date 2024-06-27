7135299726022720a17fd1115edcf071b802c354
borissvoboda <svoboda.boris@gmail.com>
2024-06-27 19:09:10 +0200

    js

---
+++ js/refresher/002/002basics_37convDataTypes.js
@@ -0,0 +1,16 @@
+let textInput = '10';
+
+let numInputInt = parseInt(textInput);
+
+console.log(numInputInt);
+
+let textInputFloat = '10.54';
+
+console.log('Float: ', parseFloat(textInputFloat));
+
+let numToString = 155;
+let stringFromNum = numToString.toString();
+
+console.log('String From Num: ', stringFromNum);
+
+// Embeddeded syntax ${variable} converts toString automatically
---
+++ js/refresher/002/44arrays.js
@@ -0,0 +1,3 @@
+var myArr = [];
+
+// new entry
