3a3fee771a73452e93ee5c80e7d2d8c1f48c7225
borissvoboda <svoboda.boris@gmail.com>
2024-07-01 15:50:55 +0200

    terraform contd

--- terraform/tutorial/README.md
+++ terraform/tutorial/README.md
@@ -1,3 +1,34 @@
 # VS CODE addons:
 
 HashiCorp Terraform
+
+# see tutorial by wahl network - beginners tutorial to Terraform with AWS
+
+on youtube
+3yo however
+
+# install terraform
+
+# create main.tf file
+
+# fill the content
+
+# RUN > terraform init
+
+# RUN > terraform fmt
+
+// terraform format
+
+# terraform validate
+
+# terraform plan
+
+// !Provider
+
+# terraform apply
+
+# ------------------
+
+# Notes
+
+! terraform.tfstate is important - registers changes
--- terraform/tutorial/main.tf
+++ terraform/tutorial/main.tf
@@ -1,21 +1,21 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 3.5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.5.0"
     }
+  }
 }
 
 provider "aws" {
-    region =  "eu-central-2"
+  region = "eu-central-1"
 }
 
 resource "aws_budgets_budget" "my_budgets_budget" {
-  name = "my_budgets_monthly"
-  budget_type = "COST"
-  limit_amount = "100.0"
-  limit_unit = "USD"
-  time_unit = "MONTHLY"
+  name              = "my_budgets_monthly"
+  budget_type       = "COST"
+  limit_amount      = "100.0"
+  limit_unit        = "USD"
+  time_unit         = "MONTHLY"
   time_period_start = "2024-07-25_00:01"
 }
