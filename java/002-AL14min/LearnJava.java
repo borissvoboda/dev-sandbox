c7c2f6505f061ee83338afa878adfb7eaf90124d
borissvoboda <svoboda.boris@gmail.com>
2024-07-20 18:01:03 +0200

    java init

---
+++ java/002-AL14min/LearnJava.java
@@ -0,0 +1,19 @@
+public class LearnJava {
+    public static void main(String[] args) {
+
+        // Primitive types
+        int a = 5;
+        char b = 'a';
+        long c = 400;
+        double d = 3.2;
+
+        // Non primitive types
+        String name = "John"; // object
+
+        name.
+
+
+
+}
+
+}
