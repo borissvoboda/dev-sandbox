b03e1f91320ecf693fb2c25ff51d3902507f71f0
borissvoboda <svoboda.boris@gmail.com>
2024-09-16 17:17:26 +0200

    array qna

---
+++ theory/primeAlgor/001-Basics/004-ArraysQnA.js
@@ -0,0 +1,15 @@
+const text = `
+In Rust there has to be specified size.
+
+In general:
+Array, you can re-allocate it; create a new array.
+
+In JS, or JavaScript you do not see underlying array.
+
+------------
+
+In Rust, when you create a new vector, its gonna create a memory buffer with the size of 5, to play with.
+Without need to reallocate. 
+The question is, what wiggling buffer is reasonable.
+
+`;
