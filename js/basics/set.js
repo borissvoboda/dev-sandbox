78c5e0cb993c6708d778c9c9ca8a47a0c59b0642
borissvoboda <svoboda.boris@gmail.com>
2024-09-18 10:00:06 +0200

    set

---
+++ js/basics/set.js
@@ -0,0 +1,7 @@
+const numbers = [1, 9, 2, 3, 4, 4, 5, 6, 7, 7, 8, 9];
+
+console.log(typeof new Set(numbers));
+
+// const uniqueNumbers = Array.from(new Set(numbers));
+
+// console.log(uniqueNumbers); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
